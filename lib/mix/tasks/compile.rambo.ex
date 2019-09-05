f86767d69328b9c259d696cac6f72e7268eaba51
Tan Jay Jun <tan@jayjun.com>
2019-09-05 18:21:47 +0800

    Use Docker to cross compile binaries

--- lib/mix/tasks/compile.rambo.ex
+++ lib/mix/tasks/compile.rambo.ex
@@ -44,46 +44,67 @@ defmodule Mix.Tasks.Compile.Rambo do
     end
   end
 
-  def run(["--unix"]) do
-    for target <- [@mac, @linux], reduce: :ok do
-      :ok -> compile(target)
+  def run(["bundled"]) do
+    with :ok <- compile(@mac),
+         :ok <- compile_in_docker(@linux),
+         :ok <- compile_in_docker(@windows) do
+      :ok
+    else
       error -> error
     end
   end
 
-  def run(["--windows"]) do
-    compile(@windows)
+  def run(_args) do
+    compile_locally()
   end
 
-  def run([]) do
-    compile()
+  defp priv_dir do
+    List.to_string(:code.priv_dir(:rambo))
   end
 
-  def compile(target \\ nil) do
-    priv_dir = List.to_string(:code.priv_dir(:rambo))
+  defp compile(target \\ nil) do
+    priv_dir = priv_dir()
 
     args =
-      if target do
-        ["build", "--release", "--target", target]
-      else
-        ["build", "--release"]
+      case target do
+        nil -> ["build", "--release"]
+        target -> ["build", "--release", "--target", target]
       end
 
     case System.cmd("cargo", args, cd: priv_dir) do
       {_output, 0} ->
-        source =
-          if target do
-            Path.join(priv_dir, "target/#{target}/release/rambo")
-          else
-            Path.join(priv_dir, "target/release/rambo")
-          end
-
-        destination = Path.join(priv_dir, executable(target))
-        File.rename!(source, destination)
+        move_executable(priv_dir, target)
         :ok
 
       {output, _exit_status} ->
         {:error, [output]}
     end
+  end
+
+  defp compile_in_docker(target) do
+    priv_dir = priv_dir()
+    tag = "rambo/" <> target
+    build = ["build", "--tag", tag, "--file", "Dockerfile." <> target, "."]
+
+    with {_output, 0} <- System.cmd("docker", build, cd: priv_dir),
+         {_output, 0} <- System.cmd("docker", ["run", "--volume", priv_dir <> ":/app", tag]) do
+      move_executable(priv_dir, target)
+      :ok
+    else
+      {output, _exit_status} -> {:error, [output]}
+    end
+  end
+
+  defp move_executable(priv_dir, target) do
+    target_executable =
+      case target do
+        nil -> "target/release/rambo"
+        @windows -> "target/#{@windows}/release/rambo.exe"
+        target -> "target/#{target}/release/rambo"
+      end
+
+    source = Path.join(priv_dir, target_executable)
+    destination = Path.join(priv_dir, executable(target))
+    File.rename!(source, destination)
   end
 end
---
+++ priv/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Initialize the wine prefix (virtual windows installation)
+export WINEPREFIX=/tmp/wine
+mkdir -p $WINEPREFIX
+# FIXME: Make the wine prefix initialization faster
+wineboot &> /dev/null
+
+# Put libstdc++ and some other mingw dlls in WINEPATH
+# This must work for x86_64 and i686
+P1=$(dirname $(find /usr -name libwinpthread-1.dll))
+export WINEPATH="$(ls -d /usr/lib/gcc/*-w64-mingw32/*posix);$P1"
+
+exec "$@"
